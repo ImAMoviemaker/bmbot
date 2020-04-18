aea5aba11590132ad5cf8b11792aa0235dc99f9d
Tim <info@timhaetti.ch>
2020-04-18 15:57:31 +0200

    asd

--- bot.py
+++ bot.py
@@ -235,8 +235,7 @@ async def on_command(ctx):
 @bot.command(name='role-msg')
 async def on_command(ctx):
     message1 = "🎬 You can assign yourself a role here 🎬\nThis will help others to quickly identify your interest and knowledge.\n\nYour occupation:\n📢  Director\n💰  Producer\n🎥 Camera Department\n🖥️  Post Production\n✍️  Writer\n👨  Hobbyist\nJust click the corresponding emoji below."
-    message2 = "\n\nYour gear:\n🖥️  Post Production Gear\n🎥  Camera Gear\n📡  Broadcasting\n\nThose roles are not pingable. You will not receive any additional pings\nJust click the corresponding emoji below."
-    await ctx.send(message1)
+    message2 = ".\n\nYour gear:\n🖥️  Post Production Gear\n🎥  Camera Gear\n📡  Broadcasting\nJust click the corresponding emoji below."
     await ctx.send(message2)
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 bot.run(TOKEN)
